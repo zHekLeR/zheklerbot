de2da16f9179bfdccf4dfc54b8feb272cd1388d9
zHekLeR <zHekLeR@hotmail.com>
2022-11-22 21:00:07 -0500

    Wordle debug

--- games/wordle.js
+++ games/wordle.js
@@ -43,7 +43,7 @@ function check(word, guess) {
             // If the character is in the word but in the wrong spot, add a yellow.
             // If the character is in the word but you already have it in the correct place, add a red.
             let split = word.split(guess.charAt(i)).join('');
-            result += ((count >= (5 - split.length))?r:y);
+            result += ((count > (5 - split.length))?r:y);
             
             // Guessed word is not the correct word.
             correct = false;
