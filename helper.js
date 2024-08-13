43e93b59df62732c8799ed9dd75e35f5a52cdcbe
Quinn DeVal <zhekler@hotmail.com>
2024-08-13 14:17:47 -0400

    Update DATABASE_URL environment variable

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@
 import pg from 'pg';
 const { Pool } = pg;
 const pool = new Pool({
-  connectionString: process.env.DATABASE,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
