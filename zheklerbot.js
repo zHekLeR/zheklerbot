7cc3e4448ec4b14fb3a710348c94052e191a21cf
zHekLeR <99208094+zHekLeR@users.noreply.github.com>
2026-09-22 19:00:28 -0400

    Maybe fix access token

--- zheklerbot.js
+++ zheklerbot.js
@@ -74,6 +74,25 @@ const account_config = {
 };
 
 
+// Create the Twitch bot.
+var bot = new tmi.Client({
+  connection: {
+    reconnect: true,
+    secure: true
+  },
+  identity: {
+    username: 'zhekler',
+    password: process.env.ACCESS_TOKEN
+  },
+  channels: []
+});
+
+// Logs the Twitch bot being initialized.
+bot.on('logon', () => {
+  console.log("Twitch bot logged on.");
+});
+
+
 // HusKerrs VIPs.
 const vips = 'davidtheslayerrr, thunderclap60, neosog, guppii, lululuvely, officialgloriouspcgr, tannerslays, itsthiccchick, craftyjoseph, thanks4dying, mateocrafter1304, hannahnicole4300, stormen, thomdez, fuzwuz, cklaas, triv, zxch, airy_z, bumbobboi, twisttedt, meerko, confire, geesh, missnaruka, gmoe003, femsteph, gdolphn, patriotic, rknhd, rogue_frank, crowder, vileagony, safecojoe, biazar, notjustjohnny, meesterhauns, kurt, midone, muffinwithnobrim, mikedrop39, bronny, swagg, stableronaldo, willo7891, hitstreak, scump, n8brotherwolf, cloakzy, chickitv, karma, soapwingo, joewo, hoffensnieg, deeksjr, tiensochill, gloliva, destroy, its_iron, imr_sa, ochocinco, magalonn, artesianbuilds, azsnakeb1t3, hasham_33, alextumay, pat_o_, nicewigg, 1chilldawg, hideouts_, scummn, momskerrs, p90queen, drawrj, jefedejeff, methodz, valorash_, tyrannymedia, lablakers24, antdavis3, almxnd, jgod_gaming, mafiia_niko, liamferrari, timthetatman, feldubb, holyman, kentb57, l3xu55, bbreadman, sinnerrrr, aydan, unrational, wagnificent, davidtheslayerrr, tommey, zsmit, drakota, mvs_11, ndolok, janegoatt'.split(', ');
 
@@ -5185,24 +5204,6 @@ var intervals = [];
 
     setInterval(function () { duelExpiration(); }, 5000);
 
-
-    // Create the Twitch bot.
-    var bot = new tmi.Client({
-      connection: {
-        reconnect: true,
-        secure: true
-      },
-      identity: {
-        username: 'zhekler',
-        password: newToken
-      },
-      channels: []
-    });
-
-    // Logs the Twitch bot being initialized.
-    bot.on('logon', () => {
-      console.log("Twitch bot logged on.");
-    });
 
     // Connect to Twitch channels.
     var updateUsers = false;
